06130303ae505a8c166076557f6649cf0fac3dcc
IngridOliveira <dev.ingridoliveira@gmail.com>
2025-08-05 21:03:57 -0300

    Inclusão do menu e função para exclusão de música

--- Python/Catálogo Musical/src/catalogo_musical.py
+++ Python/Catálogo Musical/src/catalogo_musical.py
@@ -30,8 +30,8 @@ catalogo_musical = [
 ]
 
 def add_musica():
-    nome = str(input("Insira o nome da música: ")).capitalize()
-    artista = str(input("Insira o nome do artista/banda: ")).capitalize()
+    nome = str(input("Insira o nome da música: ")).title()
+    artista = str(input("Insira o nome do artista/banda: ")).title()
     while True:
         ano_input = (input("Insira o ano de lançamento da música: "))
         if ano_input.isdigit():
@@ -39,8 +39,8 @@ def add_musica():
             break
         else:
             print("Entrada inválida. Insira apenas valores numéricos no ano!")
-    album = str(input("Insira o nome do álbum em que a música foi lançada: ")).capitalize()
-    genero = str(input("Insira o gênero da música: ")).capitalize()
+    album = str(input("Insira o nome do álbum em que a música foi lançada: ")).title()
+    genero = str(input("Insira o gênero da música: ")).title()
 
     musica = {
         "Nome" : nome,
@@ -63,7 +63,7 @@ def list_musica():
             print(f"{musica['Nome']} - {musica['Artista']} | Lançamento: {musica['Ano']} | Álbum: {musica['Álbum']}")
             
 def buscar_musicas():
-    busca = str(input("Digite o nome ou artista: ")).capitalize()
+    busca = str(input("Digite o nome ou artista: ")).title()
     resultados = [
         result for result in catalogo_musical
         if busca in result['Nome'] or busca in result['Artista']
@@ -75,6 +75,44 @@ def buscar_musicas():
     else:
         print("Não foram encontrados resultados para a busca!")
         
-"""add_musica()"""
-list_musica()
-buscar_musicas()
+def remover_musica():
+    remover = input("Insira o nome da música que deseja excluir: ").strip().title()
+    for musica in catalogo_musical:
+        if musica.get("Nome") == remover:
+            catalogo_musical.remove(musica)
+            print(f"A música '{remover}' foi excluída com sucesso!")
+            break
+    else:
+        print(f"A música '{remover}' não foi encontrada no catálogo.")
+        
+def menu():
+    while True:
+        print("\n==== Catálogo Musical ====")
+        print("1 - Adicionar músicas")
+        print("2 - Listar músicas")
+        print("3 - Buscar")
+        print("4 - Excluir")
+        print("0 - Sair")
+        
+        opcao = input("Esolha uma opção: ")
+        
+        if opcao == "1":
+            add_musica()
+        
+        elif opcao == "2":
+            list_musica()
+        
+        elif opcao == "3":
+            buscar_musicas()
+            
+        elif opcao == "4":
+            remover_musica()
+            
+        elif opcao == "0":
+            print("\nEncerrando...\n Até logo!")
+            break
+        
+        else:
+            print("\nOpção inválida. Selecione uma das opções disponíveis no menu!")
+            
+menu()
